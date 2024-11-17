ac5f093dced8c9038d2e6f17b3f56ab3e2a4e901
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-11-17 23:15:50 +0100

    add installer

---
+++ install.php
@@ -0,0 +1,42 @@
+<?php
+
+echo "Starting installation process...\n";
+
+$requiredDirs = [
+	__DIR__ . '/var',
+	__DIR__ . '/var/cache',
+	__DIR__ . '/var/logs',
+];
+
+foreach ($requiredDirs as $dir)
+{
+	if (!is_dir($dir))
+	{
+		echo "Creating directory: $dir\n";
+		if (!mkdir($dir, 0775, true) && !is_dir($dir))
+			die("Error: Unable to create directory: $dir\n");
+	}
+	else
+		echo "Directory already exists: $dir\n";
+}
+
+foreach ($requiredDirs as $dir)
+{
+	if (!is_writable($dir))
+		die("Error: Directory is not writable: $dir\n");
+
+}
+echo "Directory structure and permissions verified.\n";
+
+echo "installing composer dependencies...\n";
+$output = shell_exec('composer install 2>&1');
+echo "<pre>$output</pre>";
+
+echo "create metadata for cli ...\n";
+$output = shell_exec('php bin/cli.php --update 2>&1');
+echo "<pre>$output</pre>";
+
+
+#echo "Migrating database...\n";
+#$output = shell_exec('php bin/cli.php --site migrate-database 2>&1');
+#echo "<pre>$output</pre>";
