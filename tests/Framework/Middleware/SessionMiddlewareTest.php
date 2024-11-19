10d6039d41a69f2438fb4725e461ccb0e0574919
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-11-19 23:21:23 +0100

    add MIddleware unittests

--- tests/Framework/Middleware/SessionMiddlewareTest.php
+++ tests/Framework/Middleware/SessionMiddlewareTest.php
@@ -3,9 +3,47 @@
 namespace Tests\Framework\Middleware;
 
 use App\Framework\Middleware\SessionMiddleware;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Flash\Messages;
+use SlimSession\Helper;
 
 class SessionMiddlewareTest extends TestCase
 {
+	/**
+	 * @throws Exception
+	 */
+	#[Group('units')]
+	public function testProcessAddsSessionAndFlashAttributesToRequest(): void
+	{
+		session_start(); // Todo-> Put this shit to integration tests or find a way to not new Messages
+		$sessionMock = $this->createMock(Helper::class);
+		$requestMock = $this->createMock(ServerRequestInterface::class);
+		$responseMock = $this->createMock(ResponseInterface::class);
+		$handlerMock = $this->createMock(RequestHandlerInterface::class);
 
+		// Mock the `withAttribute` method to ensure attributes are added
+		$requestMock->expects($this->exactly(2))
+					->method('withAttribute')
+		         	->willReturnCallback(fn($key, $value) => $requestMock);
+
+
+		// Ensure the handler's `handle` method is called with the modified request
+		$handlerMock->expects($this->once())
+					->method('handle')
+					->with($requestMock)
+					->willReturn($responseMock);
+
+		$middleware = new SessionMiddleware($sessionMock);
+		$result = $middleware->process($requestMock, $handlerMock);
+
+		// Assert that the response from the handler is returned unchanged
+		$this->assertSame($responseMock, $result);
+
+		session_destroy();
+	}
 }
