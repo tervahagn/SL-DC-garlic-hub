ed7d32ffd15b84f045d424dd02c3d250b98db3cd
sagiadinos <ns@smil-control.com>
2024-11-23 14:26:04 +0100

    add gpl header to unit tests

--- tests/Unit/Framework/Core/Translate/TranslatorTest.php
+++ tests/Unit/Framework/Core/Translate/TranslatorTest.php
@@ -17,16 +17,13 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-require __DIR__ . '/../../vendor/autoload.php';
-$_ENV['APP_ENV'] = 'testing';
 
-$systemDir = realpath(__DIR__.'/../');
-$paths = [
-	'systemDir' => $systemDir,
-	'varDir' => $systemDir . '/var',
-	'cacheDir' => $systemDir . '/var/cache',
-	'logDir' => $systemDir . '/var/log',
-	'configDir' => $systemDir . '/config'
-];
-define('_TestLibPath', $systemDir);
+namespace Tests\Framework\Core\Translate;
 
+use App\Framework\Core\Translate\Translator;
+use PHPUnit\Framework\TestCase;
+
+class TranslatorTest extends TestCase
+{
+
+}
