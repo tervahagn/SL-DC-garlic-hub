6430dfc19b47a9442a825b4adec4aea783a3f755
sagiadinos <ns@smil-control.com>
2024-12-03 12:42:20 +0100

    fix tests

--- tests/Unit/Framework/Core/Locales/SessionLocaleExtractorTest.php
+++ tests/Unit/Framework/Core/Locales/SessionLocaleExtractorTest.php
@@ -2,53 +2,58 @@
 
 namespace Tests\Unit\Framework\Core\Locales;
 
+use App\Framework\Core\Locales\LocaleExtractorInterface;
 use App\Framework\Core\Locales\SessionLocaleExtractor;
 use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 use SlimSession\Helper;
 
 class SessionLocaleExtractorTest extends TestCase
 {
-	private $sessionMock;
-	private $localeExtractor;
+	private Helper $sessionMock;
+	private LocaleExtractorInterface $localeExtractor;
 
+	/**
+	 * @throws Exception
+	 */
 	#[Group('units')]
 	protected function setUp(): void
 	{
 		$this->sessionMock = $this->createMock(Helper::class);
-		$this->localeExtractor = new SessionLocaleExtractor($this->sessionMock, 'en');
+		$this->localeExtractor = new SessionLocaleExtractor($this->sessionMock, 'en_US');
 	}
 
 	#[Group('units')]
 	public function testExtractLocaleFromSession()
 	{
-		$this->sessionMock->method('get')->with('user')->willReturn(['locale' => 'de']);
+		$this->sessionMock->method('get')->with('locale', 'en_US')->willReturn('de_DE');
 
-		$whiteList = ['en', 'de', 'fr'];
+		$whiteList = ['en_US', 'de_DE', 'fr_FR'];
 		$result = $this->localeExtractor->extractLocale($whiteList);
 
-		$this->assertEquals('de', $result);
+		$this->assertEquals('de_DE', $result);
 	}
 
 	#[Group('units')]
 	public function testExtractLocaleFallbackToDefault()
 	{
-		$this->sessionMock->method('get')->with('user')->willReturn(null);
+		$this->sessionMock->method('get')->with('locale', 'en_US')->willReturn(null);
 
-		$whiteList = ['en', 'de', 'fr'];
+		$whiteList = ['en_US', 'de_DE', 'fr_FR'];
 		$result = $this->localeExtractor->extractLocale($whiteList);
 
-		$this->assertEquals('en', $result);
+		$this->assertEquals('en_US', $result);
 	}
 
 	#[Group('units')]
 	public function testExtractLocaleFallbackToDefaultForInvalidLocale()
 	{
-		$this->sessionMock->method('get')->with('user')->willReturn(['locale' => 'es']);
+		$this->sessionMock->method('get')->with('locale', 'en_US')->willReturn(['locale' => 'es_ES']);
 
-		$whiteList = ['en', 'de', 'fr'];
+		$whiteList = ['en_US', 'de_DE', 'fr_FR'];
 		$result = $this->localeExtractor->extractLocale($whiteList);
 
-		$this->assertEquals('en', $result);
+		$this->assertEquals('en_US', $result);
 	}
 }
