41dc600d1e6bd4af92ad008ed54b47d091ec95e2
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-19 22:54:27 +0100

    add tests emailrendeer

---
+++ tests/Unit/Framework/Utils/Html/EmailRendererTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\Framework\Utils\Html;
+
+use App\Framework\Utils\Html\EmailRenderer;
+use App\Framework\Utils\Html\FieldInterface;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+class EmailRendererTest extends TestCase
+{
+	#[Group('units')]
+	public function testRenderWithMinimumAttributes(): void
+	{
+		$fieldMock = $this->createMock(FieldInterface::class);
+		$fieldMock->method('getName')->willReturn('email_1');
+		$fieldMock->method('getId')->willReturn('email_1');
+		$fieldMock->method('getValue')->willReturn('test@test.kl');
+		$fieldMock->method('getValidationRules')->willReturn([]);
+		$fieldMock->method('getAttributes')->willReturn([]);
+
+		$renderer = new EmailRenderer();
+		$result = $renderer->render($fieldMock);
+
+		$expected = '<input type="email" name="email_1" id="email_1" value="test@test.kl" aria-describedby="error_email_1">';
+		$this->assertSame($expected, $result);
+	}
+}
