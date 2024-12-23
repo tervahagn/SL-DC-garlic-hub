7d1f35dbdccb261d451b3aa57d311992d4e38dd1
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-23 23:14:19 +0100

    some design and mediapool

--- public/js/mediapool/treeview.js
+++ public/js/mediapool/treeview.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function(event)
 		element: document.getElementById("mediapool-tree"),
 		source: { url: '/async/mediapool/node/0' },
 		init: (e) => {
-			e.tree.setFocus();
 		},
 		lazyLoad: function (e) {
 			return { url: '/async/mediapool/node/'+ e.node.key, params: { parentKey: e.node.key } };
@@ -12,6 +11,17 @@ document.addEventListener("DOMContentLoaded", function(event)
 		activate: (e) => {
 			document.getElementById("current-path").textContent = "/" + e.node.getPath();
 		}
+	});
+
+	const editFolderDialog = document.getElementById('editFolderDialog');
+	const addRootFolder = document.getElementById('addRootFolder');
+
+	addRootFolder.addEventListener('click', () => {
+		editFolderDialog.showModal();
+	});
+
+	editFolderDialog.addEventListener('close', () => {
+		console.log('Dialog closed with value:', dialog.returnValue);
 	});
 
 });
