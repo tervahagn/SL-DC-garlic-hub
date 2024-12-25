1bd710ef09ce436a837d7606143d46aba6c13a8e
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-25 18:56:42 +0100

    corrected route.php for async

--- config/route.php
+++ config/route.php
@@ -48,4 +48,4 @@ $app->get('/mediapool', [ShowController::class, 'show']);
 $app->get('/async/mediapool/node[/{parent_id}]', [NodesController::class, 'list']); // parent_id is optional with []
 $app->post('/async/mediapool/node', [NodesController::class, 'add']);
 $app->delete('/async/mediapool/node', [NodesController::class, 'delete']);
-$app->patch('/async/mediapool/node/{node_id}', [NodesController::class, 'edit']);
+$app->patch('/async/mediapool/node', [NodesController::class, 'edit']);
