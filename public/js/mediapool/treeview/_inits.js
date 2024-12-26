c3b5b71f713584114590d82a5d02e451ccd48a91
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-26 01:05:57 +0100

    remove init in js

---
+++ public/js/mediapool/treeview/_inits.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(event)
+{
+	let nodesModel     = new NodesModel();
+	const tree_element = document.getElementById("mediapool-tree");
+	let directoryView  = new DirectoryView(tree_element);
+	directoryView.addFilter(document.getElementById("tree_filter"));
+
+	let treeDialog = new TreeDialog(
+		document.getElementById('editFolderDialog'),
+		document.getElementById("close_dialog_button"),
+		directoryView,
+		nodesModel
+	);
+
+	document.getElementById('addRootFolder').addEventListener('click', () => {
+		treeDialog.setCurrentNode(0);
+		treeDialog.prepareShow("add_root_folder", lang);
+		treeDialog.show();
+	});
+
+	tree_element.addEventListener("contextmenu", (event) => {
+		event.preventDefault();
+		currentTreeNode = directoryView.setActiveNodeFromEventTarget(event.target);
+
+		const menu = document.querySelector('#context_menu_tree').content.cloneNode(true).firstElementChild;
+		let contextMenu    = new ContextMenu(menu, nodesModel, treeDialog);
+		contextMenu.show(event);
+
+		const editNodeElement = document.getElementById("edit_node");
+		contextMenu.addEditEvent(editNodeElement, currentTreeNode, lang);
+
+		const addNodeElement = document.getElementById("add_node");
+		contextMenu.addAddEvent(addNodeElement, currentTreeNode, lang);
+
+		const removeNodeElement = document.getElementById("remove_node");
+		contextMenu.addRemoveEvent(removeNodeElement, currentTreeNode);
+	});
+});
+
