54bc7ba0c58cea382db48233829058818c136a8c
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-29 13:52:50 +0100

    new files mediapool

---
+++ src/Modules/Mediapool/Services/UploadService.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2024 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or  modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+namespace App\Modules\Mediapool\Services;
+
+use App\Modules\Mediapool\Repositories\FilesRepository;
+use App\Modules\Mediapool\Repositories\QueueRepository;
+use App\Modules\Mediapool\Utils\MediaHandlerFactory;
+
+class UploadService
+{
+	private MediaHandlerFactory $mediaHandlerFactory;
+	private FilesRepository $mediaRepository;
+	private QueueRepository $queueRepository;
+
+	/**
+	 * @param MediaHandlerFactory $mediaHandlerFactory
+	 * @param FilesRepository     $mediaRepository
+	 * @param QueueRepository     $queueRepository
+	 */
+	public function __construct(MediaHandlerFactory $mediaHandlerFactory, FilesRepository $mediaRepository,	QueueRepository $queueRepository)
+	{
+		$this->mediaHandlerFactory = $mediaHandlerFactory;
+		$this->mediaRepository = $mediaRepository;
+		$this->queueRepository = $queueRepository;
+	}
+
+
+}
