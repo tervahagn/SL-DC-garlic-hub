80f7d158e5c143efba9f715ffdbb95dbc244f5c3
Nikolaos Sagiadinos <info@saghiadinos.de>
2024-12-29 14:19:10 +0100

    restruct tree for using in modules

--- public/js/mediapool/treeview/DirectoryView.js
+++ public/js/mediapool/treeview/DirectoryView.js
@@ -19,15 +19,17 @@
 class DirectoryView
 {
     #tree = null;
+    #tree_element = null;
     static DEBUG_LEVEL= 3;
     static SOURCE_URI   = '/async/mediapool/node/0';
     static LAZYLOAD_URI =  '/async/mediapool/node/';
 
     constructor(tree_element, current_path)
     {
+        this.#tree_element = tree_element;
         this.#tree = new mar10.Wunderbaum({
             debugLevel: DirectoryView.DEBUG_LEVEL,
-            element: tree_element,
+            element: this.#tree_element,
             source: { url: DirectoryView.SOURCE_URI },
             selectMode: "single",
             lazyLoad: function (e){
@@ -43,6 +45,27 @@ class DirectoryView
         tree_filter.addEventListener("input", (event) => {
             this.#tree.filterNodes(event.target.value, { mode: "hide" });
         })
+    }
+
+    addContextMenu(nodesModel, treeDialog, lang)
+    {
+        this.#tree_element.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            const currentTreeNode = this.setActiveNodeFromEventTarget(event.target);
+
+            const menu = document.querySelector('#context_menu_tree').content.cloneNode(true).firstElementChild;
+            let contextMenu    = new ContextMenu(menu, nodesModel, treeDialog);
+            contextMenu.show(event);
+
+            const editNodeElement = document.getElementById("edit_node");
+            contextMenu.addEditEvent(editNodeElement, currentTreeNode, lang);
+
+            const addNodeElement = document.getElementById("add_node");
+            contextMenu.addAddEvent(addNodeElement, currentTreeNode, lang);
+
+            const removeNodeElement = document.getElementById("remove_node");
+            contextMenu.addRemoveEvent(removeNodeElement, currentTreeNode);
+        });
     }
 
     setActiveNodeFromEventTarget(event_target)
--- public/js/mediapool/treeview/_inits.js
+++ public/js/mediapool/treeview/_inits.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event)
 {
 	let nodesModel     = new NodesModel();
-	const tree_element = document.getElementById("mediapool-tree");
 	let directoryView  = new DirectoryView(
-		tree_element,
+		document.getElementById("mediapool-tree"),
 		document.getElementById("current-path")
 	);
 	directoryView.addFilter(document.getElementById("tree_filter"));
@@ -14,30 +13,12 @@ document.addEventListener("DOMContentLoaded", function(event)
 		directoryView,
 		nodesModel
 	);
+	directoryView.addContextMenu(nodesModel, treeDialog, lang);
 
 	document.getElementById('addRootFolder').addEventListener('click', () => {
 		treeDialog.setCurrentNode(null);
 		treeDialog.prepareShow("add_root_folder", lang);
 		treeDialog.show();
-	});
-
-
-	tree_element.addEventListener("contextmenu", (event) => {
-		event.preventDefault();
-		currentTreeNode = directoryView.setActiveNodeFromEventTarget(event.target);
-
-		const menu = document.querySelector('#context_menu_tree').content.cloneNode(true).firstElementChild;
-		let contextMenu    = new ContextMenu(menu, nodesModel, treeDialog);
-		contextMenu.show(event);
-
-		const editNodeElement = document.getElementById("edit_node");
-		contextMenu.addEditEvent(editNodeElement, currentTreeNode, lang);
-
-		const addNodeElement = document.getElementById("add_node");
-		contextMenu.addAddEvent(addNodeElement, currentTreeNode, lang);
-
-		const removeNodeElement = document.getElementById("remove_node");
-		contextMenu.addRemoveEvent(removeNodeElement, currentTreeNode);
 	});
 });
 
