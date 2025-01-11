b1078a96d507d761d2c98073d8c9f054f66274db
sagiadinos <ns@smil-control.com>
2025-01-11 02:26:20 +0100

    finilize aotlogin implementation

--- src/Framework/Middleware/AuthMiddleware.php
+++ src/Framework/Middleware/AuthMiddleware.php
@@ -21,7 +21,13 @@
 
 namespace App\Framework\Middleware;
 
-use App\Framework\Core\Session\SessionStorage;
+use App\Framework\Core\Cookie;
+use App\Framework\Core\Session;
+use App\Framework\Exceptions\FrameworkException;
+use App\Framework\Exceptions\UserException;
+use App\Modules\Auth\AuthService;
+use Doctrine\DBAL\Exception;
+use Phpfastcache\Exceptions\PhpfastcacheSimpleCacheException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -30,33 +36,82 @@ use Slim\Psr7\Response;
 
 class AuthMiddleware implements MiddlewareInterface
 {
+
+	private AuthService $authService;
+
 	private array $publicRoutes = [
-		'/login',
-		'/set-locales',
-		'/register',
-		'/reset-password',
-		'/legals',
-		'/privacy',
-		'/terms',
-		'/cms'
+		'set-locales',
+		'register',
+		'reset-password',
+		'legals',
+		'privacy',
+		'terms',
+		'cms'
 	];
 
+	/**
+	 * @param AuthService $authService
+	 */
+	public function __construct(AuthService $authService)
+	{
+		$this->authService = $authService;
+	}
+
+	/**
+	 * @throws UserException
+	 * @throws PhpfastcacheSimpleCacheException
+	 * @throws FrameworkException
+	 * @throws Exception
+	 */
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
 	{
-		// if public route then skip
-		// todo: check if an anonymous middleware would be more efficient
+		// check for public route and skip authentication
 		preg_match('~^/([^/]+)~', $request->getUri()->getPath(), $matches);
-		if (!isset($matches[0]) || in_array($matches[0], $this->publicRoutes, true))
+		if (!isset($matches[1]) || in_array($matches[1], $this->publicRoutes, true))
 			return $handler->handle($request);
 
-		/** @var SessionStorage $session */
+		/** @var Session $session */
 		$session = $request->getAttribute('session');
+		if ($session === null)
+			return $this->respondNonAuth($matches[1], $request, $handler);
+
 		if (!$session->exists('user'))
 		{
-			$response = new Response();
-			return $response->withHeader('Location', '/login')->withStatus(302);
+			/** @var Cookie $cookie */
+			$cookie     = $request->getAttribute('cookie');
+			if ($cookie === null)
+				return $this->respondNonAuth($matches[1], $request, $handler);
+
+			$userEntity = null;
+			if ($cookie->hasCookie(AuthService::COOKIE_NAME_AUTO_LOGIN))
+				$userEntity = $this->authService->loginByCookie();
+
+			if ($userEntity === null)
+				return $this->respondNonAuth($matches[1], $request, $handler);
+
+			$main_data = $userEntity->getMain();
+			$session->set('user', $main_data);
+			$session->set('locale', $main_data['locale']);
 		}
 
-		return $handler->handle($request);
+		return $this->respondAuth($matches[1], $request, $handler);
 	}
+
+	private function respondAuth(string $path, ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+	{
+		$response = new Response();
+		if ($path === 'login')
+			return $response->withHeader('Location', '/')->withStatus(302);
+		else
+			return $handler->handle($request);
+	}
+	private function respondNonAuth(string $path, ServerRequestInterface $request, RequestHandlerInterface $handler):	ResponseInterface
+	{
+		$response = new Response();
+		if ($path === 'login')
+			return $handler->handle($request);
+		else
+			return $response->withHeader('Location', '/login')->withStatus(302);
+	}
+
 }
