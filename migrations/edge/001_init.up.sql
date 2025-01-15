fc431b38dc61dc20e584270e6a8a7053adc07cc9
sagiadinos <ns@smil-control.com>
2025-01-15 05:07:48 +0100

    fix migration sql

--- migrations/edge/001_init.up.sql
+++ migrations/edge/001_init.up.sql
@@ -93,7 +93,7 @@ INSERT INTO mediapool_nodes (root_id, parent_id, level, root_order, lft, rgt, UI
 VALUES (1, 0, 1, 1, 1, 10, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'public');
 
 INSERT INTO mediapool_nodes (root_id, parent_id, level, root_order, lft, rgt, UID, is_public, last_updated,  create_date, name)
- VALUES (2, 0, 2, 2, 1, 4, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'user');
+ VALUES (2, 0, 1, 2, 1, 4, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'user');
 
 -- set some default dirs under public
 INSERT INTO mediapool_nodes (root_id, parent_id, level, root_order, lft, rgt, UID, is_public, last_updated, create_date, name)
