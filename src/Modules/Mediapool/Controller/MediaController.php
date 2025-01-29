856b72e41976f1064b54ac63ec1c5b4de686ebff
sagiadinos <ns@smil-control.com>
2025-01-29 23:03:43 +0100

    optimize MediaController

--- src/Modules/Mediapool/Controller/MediaController.php
+++ src/Modules/Mediapool/Controller/MediaController.php
@@ -1,27 +1,7 @@
 <?php
-/*
- garlic-hub: Digital Signage Management Platform
-
- Copyright (C) 2024 Nikolaos Sagiadinos <garlic@saghiadinos.de>
- This file is part of the garlic-hub source code
-
- This program is free software: you can redistribute it and/or  modify
- it under the terms of the GNU Affero General Public License, version 3,
- as published by the Free Software Foundation.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 
 namespace App\Modules\Mediapool\Controller;
 
-use App\Framework\Core\Session;
 use App\Modules\Mediapool\Services\MediaService;
 use Doctrine\DBAL\Exception;
 use Psr\Http\Message\ResponseInterface;
@@ -30,7 +10,6 @@ use Psr\Http\Message\ServerRequestInterface;
 class MediaController
 {
 	private MediaService $mediaService;
-	private int $UID;
 
 	public function __construct(MediaService $mediaService)
 	{
@@ -40,25 +19,14 @@ class MediaController
 	/**
 	 * @throws Exception
 	 */
-	public function list(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+	public function list(ResponseInterface $response, array $args): ResponseInterface
 	{
-		if (!$this->hasRights($request->getAttribute('session')))
-		{
-			$response->getBody()->write(json_encode([]));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(403);
-		}
-
-		$node_id = (array_key_exists('node_id', $args)) ? (int) $args['node_id'] : 0;
+		$node_id = $args['node_id'] ?? 0;
 		if ($node_id === 0)
-		{
-			$response->getBody()->write(json_encode(['success' => false, 'error_message' => 'node is missing']));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-		}
+			return $this->jsonResponse($response, ['success' => false, 'error_message' => 'node is missing']);
 
 		$media_list = $this->mediaService->listMedia($node_id);
-		$response->getBody()->write(json_encode(['success' => true, 'media_list' => $media_list]));
-
-		return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+		return $this->jsonResponse($response, ['success' => true, 'media_list' => $media_list]);
 	}
 
 	/**
@@ -66,23 +34,13 @@ class MediaController
 	 */
 	public function delete(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
 	{
-		if (!$this->hasRights($request->getAttribute('session')))
-		{
-			$response->getBody()->write(json_encode([]));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(403);
-		}
-
 		$bodyParams = $request->getParsedBody();
-		if (!array_key_exists('media_id', $bodyParams))
-		{
-			$response->getBody()->write(json_encode(['success' => false, 'error_message' => 'media id is missing']));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-		}
+		if (!isset($bodyParams['media_id']))
+			return $this->jsonResponse($response, ['success' => false, 'error_message' => 'media id is missing']);
 
-		$this->mediaService->setUID($this->UID);
+		$this->mediaService->setUID($request->getAttribute('session')->get('user')['UID']);
 		$count = $this->mediaService->deleteMedia($bodyParams['media_id']);
-		$response->getBody()->write(json_encode(['success' => true, 'data' => ['deleted_media' => $count]]));
-		return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+		return $this->jsonResponse($response, ['success' => true, 'data' => ['deleted_media' => $count]]);
 	}
 
 	/**
@@ -90,30 +48,13 @@ class MediaController
 	 */
 	public function move(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
 	{
-		if (!$this->hasRights($request->getAttribute('session')))
-		{
-			$response->getBody()->write(json_encode([]));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(403);
-		}
-
 		$bodyParams = $request->getParsedBody();
-		if (!array_key_exists('media_id', $bodyParams))
-		{
-			$response->getBody()->write(json_encode(['success' => false, 'error_message' => 'media id is missing']));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-		}
+		if (!isset($bodyParams['media_id']) || !isset($bodyParams['node_id']))
+			return $this->jsonResponse($response, ['success' => false, 'error_message' => 'media id or node is missing']);
 
-		$bodyParams = $request->getParsedBody();
-		if (!array_key_exists('node_id', $bodyParams))
-		{
-			$response->getBody()->write(json_encode(['success' => false, 'error_message' => 'node is missing']));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-		}
-
-		$this->mediaService->setUID($this->UID);
+		$this->mediaService->setUID($request->getAttribute('session')->get('user')['UID']);
 		$count = $this->mediaService->moveMedia($bodyParams['media_id'], $bodyParams['node_id']);
-		$response->getBody()->write(json_encode(['success' => true, 'data' => ['deleted_media' => $count]]));
-		return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+		return $this->jsonResponse($response, ['success' => true, 'data' => ['deleted_media' => $count]]);
 	}
 
 	/**
@@ -121,31 +62,19 @@ class MediaController
 	 */
 	public function clone(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
 	{
-		if (!$this->hasRights($request->getAttribute('session')))
-		{
-			$response->getBody()->write(json_encode([]));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(403);
-		}
-
 		$bodyParams = $request->getParsedBody();
-		if (!array_key_exists('media_id', $bodyParams))
-		{
-			$response->getBody()->write(json_encode(['success' => false, 'error_message' => 'media id is missing']));
-			return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
-		}
+		if (!isset($bodyParams['media_id']))
+			return $this->jsonResponse($response, ['success' => false, 'error_message' => 'media id is missing']);
 
-		$this->mediaService->setUID($this->UID);
+		$this->mediaService->setUID($request->getAttribute('session')->get('user')['UID']);
 		$new_media = $this->mediaService->cloneMedia($bodyParams['media_id']);
-		$response->getBody()->write(json_encode(['success' => true, 'new_media' => $new_media]));
+		return $this->jsonResponse($response, ['success' => true, 'new_media' => $new_media]);
+	}
+
+	private function jsonResponse(ResponseInterface $response, array $data): ResponseInterface
+	{
+		$response->getBody()->write(json_encode($data));
 		return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
 	}
 
-	private function hasRights(Session $session): bool
-	{
-		$ret = $session->exists('user');
-		if ($ret)
-			$this->UID = $session->get('user')['UID'];
-
-		return $ret;
-	}
 }
