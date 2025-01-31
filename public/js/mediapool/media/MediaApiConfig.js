b74f2e5fe2b0eb14a27ca249246e190b7a7f9c83
sagiadinos <ns@smil-control.com>
2025-01-31 18:19:44 +0100

    add MediaApiConfig.js

---
+++ public/js/mediapool/media/MediaApiConfig.js
@@ -0,0 +1,28 @@
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2025 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or  modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+export const MediaApiConfig = {
+	BASE_URI: "/async/mediapool/media",
+	LIST_URI: "/async/mediapool/media/",
+	MOVE_URI: "/async/mediapool/media/move",
+	EDIT_URI: "/async/mediapool/media/edit",
+	CLONE_URI: "/async/mediapool/media/clone",
+	PYR_CSS_PATH: "/css/external/plyr.css",
+	PYR_JS_PATH: "/js/external/plyr.js"
+};
