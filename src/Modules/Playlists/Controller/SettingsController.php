60845ccc76242a8213add66d9aa802e5e619e612
sagiadinos <ns@smil-control.com>
2025-02-20 13:10:45 +0100

    SettingCntroller first try

--- src/Modules/Playlists/Controller/SettingsController.php
+++ src/Modules/Playlists/Controller/SettingsController.php
@@ -20,14 +20,28 @@
 
 namespace App\Modules\Playlists\Controller;
 
+use App\Framework\Core\Translate\Translator;
+use App\Framework\Exceptions\CoreException;
+use App\Framework\Exceptions\FrameworkException;
+use App\Framework\Exceptions\ModuleException;
+use App\Framework\Utils\Html\FieldInterface;
+use App\Framework\Utils\Html\FieldType;
 use App\Framework\Utils\Html\FormBuilder;
+use App\Modules\Playlists\PlaylistMode;
 use App\Modules\Playlists\Services\PlaylistsService;
+use Doctrine\DBAL\Exception;
+use Phpfastcache\Exceptions\PhpfastcacheSimpleCacheException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\SimpleCache\InvalidArgumentException;
 
 class SettingsController
 {
 	private readonly FormBuilder $formBuilder;
 	private readonly PlaylistsService $playlistsService;
-
+	private Translator $translator;
+	private int $UID;
+	private string $playlistMode;
 	/**
 	 * @param FormBuilder $formBuilder
 	 * @param PlaylistsService $playlistsService
@@ -38,10 +52,134 @@ class SettingsController
 		$this->playlistsService = $playlistsService;
 	}
 
-
-	public function show()
+	/**
+	 * @throws CoreException
+	 * @throws FrameworkException
+	 * @throws Exception
+	 * @throws ModuleException
+	 * @throws PhpfastcacheSimpleCacheException
+	 * @throws InvalidArgumentException
+	 */
+	public function show(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
 	{
-		echo 'test';
+		$this->translator = $request->getAttribute('translator');
+		$session    = $request->getAttribute('session');
+		$this->UID  = $session->get('user')['UID'];
+
+		$this->playlistsService->setUID($this->UID);
+
+		$playlist_id = $args['playlist_id'] ?? 0;
+		$this->playlistMode = $args['playlist_mode'] ?? 'master';
+
+
+		$playlist = [];
+		if ($playlist_id > 0)
+			$playlist = $this->playlistsService->loadPlaylistForEdit($playlist_id);
+
+		$hiddenElements = [];
+		$formElements = [];
+
+		/** @var FieldInterface $element */
+		foreach ($this->createForm($playlist) as $key => $element)
+		{
+			if ($key === 'csrf_token')
+			{
+				$hiddenElements[] = [
+					'HIDDEN_HTML_ELEMENT'        => $this->formBuilder->renderField($element)
+				];
+				continue;
+			}
+
+			$formElements[] = [
+				'HTML_ELEMENT_ID'    => $element->getId(),
+				'LANG_ELEMENT_NAME'  => $element->getTranslatedName(),
+				'ELEMENT_MUST_FIELD' => '', //$element->getAttribute('required') ? '*' : '',
+				'HTML_ELEMENT'       => $this->formBuilder->renderField($element)
+			];
+		}
+		$data = [
+			'main_layout' => [
+				'LANG_PAGE_TITLE' => $this->translator->translate('settings', 'playlists'),
+				'additional_css' => ['/css/playlists/settings.css']
+			],
+			'this_layout' => [
+				'template' => 'generic/edit', // Template-name
+				'data' => [
+					'LANG_PAGE_HEADER' => 'User Options',
+					'SITE' => '/playlists/settings',
+					'element_hidden' => $hiddenElements,
+					'form_element' => $formElements,
+					'form_button' => [
+						[
+							'ELEMENT_BUTTON_TYPE' => 'submit',
+							'ELEMENT_BUTTON_NAME' => 'submit',
+							'LANG_ELEMENT_BUTTON' => $this->translator->translate('save', 'main')
+						]
+					]
+				]
+			]
+		];
+		$response->getBody()->write(serialize($data));
+
+		return $response->withHeader('Content-Type', 'text/html');
+
 	}
+
+
+	/**
+	 * @throws FrameworkException
+	 * @throws CoreException
+	 * @throws Exception
+	 * @throws \Phpfastcache\Exceptions\PhpfastcacheSimpleCacheException
+	 * @throws \Psr\SimpleCache\InvalidArgumentException
+	 */
+	private function createForm(array $playlist): array
+	{
+		$form       = [];
+		$rules      = ['required' => true, 'minlength' => 2];
+
+		$form['playlist_name'] = $this->formBuilder->createField([
+			'type' => FieldType::TEXT,
+			'id' => 'playlist_name',
+			'name' => 'playlist_name',
+			'translated_name' => $this->translator->translate('playlist_name', 'playlists'),
+			'value' => $playlist['name'] ?? '',
+			'rules' => $rules,
+			'default_value' => ''
+		]);
+
+		$form['UID'] = $this->formBuilder->createField([
+			'type' => FieldType::AUTOCOMPLETE,
+			'id' => 'UID',
+			'name' => 'UID',
+			'translated_name' => $this->translator->translate('owner', 'main'),
+			'value' => $playlist['UID'] ?? $this->UID,
+			'default_value' => ''
+		]);
+
+		if ($this->playlistsService->isModuleadmin() &&
+			($this->playlistMode === PlaylistMode::MASTER->value || $this->playlistMode === PlaylistMode::INTERNAL->value))
+		{
+			$form['time_limit'] = $this->formBuilder->createField([
+				'type' => FieldType::NUMBER,
+				'id' => 'time_limit',
+				'name' => 'time_limit',
+				'translated_name' => $this->translator->translate('time_limit', 'playlists'),
+				'value' => $playlist['time_limit'] ?? 0,
+				'min'   => 0,
+				'default_value' => ''
+			]);
+		}
+
+		$form['csrf_token'] = $this->formBuilder->createField([
+			'type' => FieldType::CSRF,
+			'id' => 'csrf_token',
+			'name' => 'csrf_token',
+		]);
+
+		return $form;
+
+	}
+
 
 }
