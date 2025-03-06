938748e7e6b2690e7b5a47e24e3c4fb8455c1364
sagiadinos <ns@smil-control.com>
2025-03-06 08:40:09 +0100

    paginator

---
+++ src/Framework/Utils/Paginator/Creator.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2025 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or  modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace App\Framework\Utils\Paginator;
+
+class Creator
+{
+	private int $currentPage;
+	private int $itemsPerPage;
+	private int $totalItems;
+	private bool $usePager;
+	private bool $shortened;
+	private array $pagerLinks;
+
+	public function init(int $currentPage, int $itemsPerPage, int $totalItems, bool $usePager = false, bool $shortened = true): static
+	{
+		$this->currentPage = max(1, $currentPage);
+		$this->itemsPerPage = max(1, $itemsPerPage);
+		$this->totalItems = max(0, $totalItems);
+		$this->usePager = $usePager;
+		$this->shortened = $shortened;
+
+		return $this;
+	}
+
+	public function getPagerLinks(): array
+	{
+		return $this->pagerLinks;
+	}
+
+	public function buildPagerLinks(): static
+	{
+		$this->pagerLinks = [];
+		$maxPages = max(1, (int) ceil($this->totalItems / $this->itemsPerPage));
+
+		if ($this->usePager && $this->currentPage > 1)
+		{
+			$this->pagerLinks[] = ['name' => '«', 'page' => 1];
+			$this->pagerLinks[] = ['name' => '‹', 'page' => $this->currentPage - 1];
+		}
+
+		for ($i = 1; $i <= $maxPages; $i++)
+		{
+			if (!$this->shortened || $this->isPageInRange($i, $maxPages))
+				$this->pagerLinks[] = ['name' => (string) $i, 'page' => $i, 'active' => ($i === $this->currentPage)];
+		}
+
+		if ($this->usePager && $this->currentPage < $maxPages)
+		{
+			$this->pagerLinks[] = ['name' => '›', 'page' => $this->currentPage + 1];
+			$this->pagerLinks[] = ['name' => '»', 'page' => $maxPages];
+		}
+
+		return $this;
+	}
+
+	private function isPageInRange(int $page, int $maxPages): bool
+	{
+		return $page <= 3 || $page >= ($maxPages - 2) || abs($this->currentPage - $page) <= 2;
+	}
+}
---
+++ src/Framework/Utils/Paginator/PaginatorService.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2025 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or  modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace App\Framework\Utils\Paginator;
+
+class PaginatorService
+{
+	private Creator $creator;
+	private Renderer $renderer;
+	private array $pagerLinks;
+
+	/**
+	 * @param Creator $creator
+	 * @param Renderer $renderer
+	 */
+	public function __construct(Creator $creator, Renderer $renderer)
+	{
+		$this->creator = $creator;
+		$this->renderer = $renderer;
+	}
+
+	public function create(int $currentPage, int $itemsPerPage, int $totalItems, bool $usePager = false, bool $shortened = true): void
+	{
+		$this->pagerLinks = $this->creator->init($currentPage, $itemsPerPage, $totalItems, $usePager, $shortened)
+			->buildPagerLinks()
+			->getPagerLinks();
+	}
+
+	public function render(string $site, string $sortColumn, string $sortOrder, string $elementsPerPage): array
+	{
+		return $this->renderer->render($this->pagerLinks, $site, $sortColumn, $sortOrder, $elementsPerPage);
+	}
+
+
+}
