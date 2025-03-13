4cdf0312078c8bdb46700826a5288f7bcda18e05
sagiadinos <ns@smil-control.com>
2025-03-13 10:29:44 +0100

    correct orderby test

--- tests/Unit/Modules/Mediapool/Repositories/FilesRepositoryTest.php
+++ tests/Unit/Modules/Mediapool/Repositories/FilesRepositoryTest.php
@@ -85,7 +85,7 @@ class FilesRepositoryTest extends TestCase
 
 		$this->queryBuilderMock ->expects($this->once())
 			->method('leftJoin')
-			->with('mediapool_files', 'user_main', 'user_main', 'user_main.UID=mediapool_files.UID')
+			->with('mediapool_files', 'user_main', '', 'user_main.UID=mediapool_files.UID')
 			->willReturnSelf();
 
 		$this->queryBuilderMock ->expects($this->once())->method('executeQuery')->willReturn($result);
@@ -142,12 +142,12 @@ class FilesRepositoryTest extends TestCase
 
 		$this->queryBuilderMock ->expects($this->once())
 			->method('leftJoin')
-			->with('mediapool_files', 'user_main', 'user_main', 'user_main.UID=mediapool_files.UID')
+			->with('mediapool_files', 'user_main', '', 'user_main.UID=mediapool_files.UID')
 			->willReturnSelf();
 
 		$this->queryBuilderMock ->expects($this->once())
-			->method('orderBy')
-			->with('upload_time DESC')
+			->method('addOrderBy')
+			->with('upload_time', 'DESC')
 			->willReturnSelf();
 
 		$this->queryBuilderMock ->expects($this->once())
