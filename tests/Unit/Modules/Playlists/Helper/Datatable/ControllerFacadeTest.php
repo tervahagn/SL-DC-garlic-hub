245cc454d7d848be7e26331abb17a1a379622e55
sagiadinos <ns@smil-control.com>
2025-03-26 13:38:45 +0100

    ShowDatatableController Test Playlists

---
+++ tests/Unit/Modules/Playlists/Helper/Datatable/ControllerFacadeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit\Modules\Playlists\Helper\Datatable;
+
+use App\Modules\Playlists\Helper\Datatable\ControllerFacade;
+use PHPUnit\Framework\TestCase;
+
+class ControllerFacadeTest extends TestCase
+{
+
+}
