a5150817b869233efd0f637534ad811f7a1269b7
sagiadinos <ns@smil-control.com>
2025-03-27 12:30:30 +0100

    update AbstracTAclValidator tests

--- tests/Unit/Framework/Core/Acl/AbstractAclValidatorTest.php
+++ tests/Unit/Framework/Core/Acl/AbstractAclValidatorTest.php
@@ -38,4 +38,42 @@ class AbstractAclValidatorTest extends TestCase
 		$this->aclHelperMock->expects($this->once())->method('getConfig')->willReturn($configMock);
 		$this->assertSame($configMock, $this->aclValidator->getConfig());
 	}
+
+	/**
+	 * @throws Exception
+	 */
+	#[Group('units')]
+	public function testIsSimpleAdminReturnsTrueWhenIsModuleAdmin()
+	{
+		$this->aclHelperMock->expects($this->once())->method('isModuleAdmin')->with(1, 'testModule')->willReturn(true);
+		$this->aclHelperMock->expects($this->never())->method('isSubAdmin');
+
+		$this->assertTrue($this->aclValidator->isSimpleAdmin(1));
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	#[Group('units')]
+	public function testIsSimpleAdminReturnsTrueWhenIsSubAdmin()
+	{
+		$this->aclHelperMock->expects($this->once())->method('isModuleAdmin')->with(1, 'testModule')->willReturn(false);
+		$this->aclHelperMock->expects($this->once())->method('isSubAdmin')->with(1, 'testModule')->willReturn(true);
+
+		$this->assertTrue($this->aclValidator->isSimpleAdmin(1));
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	#[Group('units')]
+	public function testIsSimpleAdminReturnsFalse()
+	{
+		$this->aclHelperMock->expects($this->once())->method('isModuleAdmin')->with(1, 'testModule')->willReturn(false);
+		$this->aclHelperMock->expects($this->once())->method('isSubAdmin')->with(1, 'testModule')->willReturn(false);
+
+		$this->assertFalse($this->aclValidator->isSimpleAdmin(1));
+	}
+
+
 }
