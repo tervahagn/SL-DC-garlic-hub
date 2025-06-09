282283c0e5ba23d8e1364cfafcdd0d02a35d8a7d
sagiadinos <ns@smil-control.com>
2025-06-09 23:29:06 +0200

    add tests for PlayerDashboard

---
+++ tests/Unit/Modules/Player/Dashboard/PlayerDashboardTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Unit\Modules\Player\Dashboard;
+
+use App\Framework\Core\Translate\Translator;
+use App\Modules\Player\Dashboard\PlayerDashboard;
+use App\Modules\Player\Services\PlayerService;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+
+class PlayerDashboardTest extends TestCase
+{
+	private PlayerDashboard $playerDashboard;
+	private PlayerService $playerServiceMock;
+	private Translator $translatorMock;
+
+	/**
+	 * @throws Exception
+	 */
+	protected function setUp(): void
+	{
+		$this->playerServiceMock = $this->createMock(PlayerService::class);
+		$this->translatorMock = $this->createMock(Translator::class);
+
+		$this->playerDashboard = new PlayerDashboard(
+			$this->playerServiceMock,
+			$this->translatorMock
+		);
+	}
+
+	#[Group('units')]
+	public function testGetId(): void
+	{
+		$result = $this->playerDashboard->getId();
+		$this->assertSame('player', $result);
+	}
+
+	#[Group('units')]
+	public function testGetTitle(): void
+	{
+		$this->translatorMock
+			->expects($this->once())
+			->method('translate')
+			->with('dashboard', 'player')
+			->willReturn('Player Dashboard');
+
+		$result = $this->playerDashboard->getTitle();
+
+		$this->assertSame('Player Dashboard', $result);
+	}
+
+	#[Group('units')]
+	public function testRenderContent(): void
+	{
+		$data = [
+			'active' => 10,
+			'pending' => 5,
+			'inactive' => 3,
+		];
+
+		$this->playerServiceMock
+			->expects($this->once())
+			->method('findAllForDashboard')
+			->willReturn($data);
+
+		$this->translatorMock
+			->expects($this->exactly(3))
+			->method('translate')
+			->willReturnMap([
+				['count_active', 'player', [], 'Active Players'],
+				['count_pending', 'player', [], 'Pending Players'],
+				['count_inactive', 'player', [], 'Inactive Players'],
+			]);
+
+		$result = $this->playerDashboard->renderContent();
+
+		$expected = '<ul>
+	<li><strong>Active Players:</strong><span>10</span></li>
+	<li><strong>Pending Players:</strong><span>5</span></li>
+	<li><strong>Inactive Players:</strong><span>3</span></li>
+</ul>';
+
+		$this->assertSame($expected, $result);
+	}
+}
