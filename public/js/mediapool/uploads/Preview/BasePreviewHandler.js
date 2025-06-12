30e2ca976b0e4f06d0f4e5f269883535fcb89a47
sagiadinos <ns@smil-control.com>
2025-06-12 18:49:53 +0200

    name source of uuid alternative

--- public/js/mediapool/uploads/Preview/BasePreviewHandler.js
+++ public/js/mediapool/uploads/Preview/BasePreviewHandler.js
@@ -118,9 +118,10 @@ export class BasePreviewHandler
 
 	generateUUID()
 	{
-		if (location.protocol === 'https:')
+		if (location.protocol === "https:")
 			return crypto.randomUUID();
 
+		// code from https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid/2117523#2117523
 		return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
 			(+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
 		);
