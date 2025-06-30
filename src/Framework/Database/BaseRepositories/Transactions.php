6c4827ce0773138937a6dbace2151511ab838f13
sagiadinos <ns@smil-control.com>
2025-07-01 00:12:01 +0200

    add transaction class to eleminate TransactionTrait

---
+++ src/Framework/Database/BaseRepositories/Transactions.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ garlic-hub: Digital Signage Management Platform
+
+ Copyright (C) 2025 Nikolaos Sagiadinos <garlic@saghiadinos.de>
+ This file is part of the garlic-hub source code
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License, version 3,
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+namespace App\Framework\Database\BaseRepositories;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
+
+readonly class Transactions
+{
+	private Connection $connection;
+
+	public function __construct(Connection $connection)
+	{
+		$this->connection = $connection;
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function begin(): void
+	{
+		$this->connection->beginTransaction();
+	}
+
+	public function isActive(): bool
+	{
+		return $this->connection->isTransactionActive();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function commit(): void
+	{
+		$this->connection->commit();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public function rollBack(): void
+	{
+		$this->connection->rollBack();
+	}
+
+}
