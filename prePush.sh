be889e837b10f811f35640f33042f7613da8ec3c
sagiadinos <ns@smil-control.com>
2025-07-09 19:08:55 +0200

    move xdebug coverage to prePush.sh for faster testing

--- prePush.sh
+++ prePush.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+XDEBUG_MODE=coverage
 php vendor/bin/phpunit --coverage-html public/clover/ --coverage-clover  public/clover/clover.xml
 vendor/bin/phpstan analyze
 vendor/bin/coverage-badge public/clover/clover.xml misc/coverage.svg coverage
