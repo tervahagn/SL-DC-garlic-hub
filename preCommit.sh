066fc016ba462c048dcc23643321a59dadca2966
sagiadinos <ns@smil-control.com>
2025-07-09 20:33:15 +0200

    change name prePush to preCommit

--- preCommit.sh
+++ preCommit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# execute this script before every commit
+
+# shellcheck disable=SC2034 # needed not to optimize testing speed look at https://thephp.cc/articles/pcov-or-xdebug
 XDEBUG_MODE=coverage
 php vendor/bin/phpunit --coverage-html public/clover/ --coverage-clover  public/clover/clover.xml
 vendor/bin/phpstan analyze
